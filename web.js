4887b2233b6a7ce7c2e4ff50835fb42423a50685
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-01-10 00:24:25 +0530

    Modified index.js to web.js for heroku deployment and installed heroku cli and modified package.json file

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { inputConverter } = require("./inputOutputConverter");
 const methodController = require("./controller");
-const product = require("./api/product");
+const router = require("./router");
  
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use("/api/product", product);
+app.use("/", router);
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
