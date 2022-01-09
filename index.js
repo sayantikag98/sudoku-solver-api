139e9e7159663f84c97cf7114abec6a6785387e7
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-01-09 22:50:30 +0530

    removed router routes and added in index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const { inputConverter } = require("./inputOutputConverter");
+const methodController = require("./controller");
+
 const app = express();
 const PORT = 3000;
-const router = require("./router");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use("/", router);
+
+app.get("/", (req, res) => {
+    res.send("Sudoku Solver API");
+});
+
+app.post("/", (req, res) => {
+    const board = inputConverter(req.body.board);
+    methodController(board, res);
+});
+
+app.get("/:id", (req, res) => {
+    const board = inputConverter(req.params.id);
+    methodController(board, res);
+});
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
